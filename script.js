133a1327408183f2645bf90d7586452d253b4c80
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-08-06 20:33:30 +0300

    Finished web-app

---
+++ script.js
@@ -0,0 +1,48 @@
+function handleFormSubmit(e) {
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    const genre = document.querySelector('#genre').value;
+    const releaseYear = document.querySelector('#releaseYear').value;
+    const isWatched = document.querySelector('#isWatched').checked;
+
+
+    const film = {
+        title,
+        genre,
+        releaseYear,
+        isWatched
+    }
+
+    addFilmToLocaleStorage()
+}
+
+function addFilmToLocaleStorage() {
+    const films = JSON.parse(localStorage.getItem('films')) || [];
+    films.push(film)
+    localStorage.setItem('films', JSON.stringify(films));
+    
+    renderTable();
+}
+
+function renderTable() {
+    const films = JSON.parse(localStorage.getItem('films')) || [];
+    const filmTableBody = document.querySelector('#film-tbody');
+    filmTableBody.innerHTML = "";
+
+    films.forEach((film) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+
+            <td>${film.title}</td>
+            <td>${film.genre}</td>      
+            <td>${film.releaseYear}</td>
+            <td>${film.isWatched ? "Да" : "Нет"}</td>
+        `;
+
+        filmTableBody.appendChild(row);
+    });
+}
+
+document.querySelector('#film-form').addEventListener("submit", handleFormSubmit);
+
+renderTable();
