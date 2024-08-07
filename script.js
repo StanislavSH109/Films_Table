ab0935eeefce6ad8db870c0f8e01105fd9ade04d
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-08-07 22:03:20 +0300

    Changed code and test localStorage

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-function handleFormSubmit(e) {
-    e.preventDefault();
-    const title = document.querySelector('#title').value;
-    const genre = document.querySelector('#genre').value;
-    const releaseYear = document.querySelector('#releaseYear').value;
-    const isWatched = document.querySelector('#isWatched').checked;
-
-
-    const film = {
-        title,
-        genre,
-        releaseYear,
-        isWatched
-    }
-
-    addFilmToLocaleStorage(film)
+const films = { 
+    title,
+    genre,
+    releaseYear,
 }
 
-function addFilmToLocaleStorage(film) {
-    const films = JSON.parse(localStorage.getItem('films')) || [];
-    films.push(film)
-    localStorage.setItem('films', JSON.stringify(films));
-    
-    renderTable();
-}
+films.title = 'Matrix';
+films.genre =  'Action';
+films.releaseYear = '1999';
 
-function renderTable() {
-    const films = JSON.parse(localStorage.getItem('films')) || [];
-    const filmTableBody = document.querySelector('#film-tbody');
-    filmTableBody.innerHTML = "";
+const dataJS = JSON.stringify(films); // {"title":"Matrix","genre":"Action","releaseYear":"1999"}
+// ; // устанавливает ключ в localeStorage 'FILMS' и подставляет данные из dataJS
+const parseJS = JSON.parse(localStorage.getItem('FILMS')) || [];
+parseJS.push(films)
+console.log(parseJS);
+localStorage.setItem('FILMS', JSON.stringify(parseJS))
 
-    films.forEach((film) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+// function handleFormSubmit(e) {
+//     e.preventDefault();
+//     const title = document.querySelector('#title').value;
+//     const genre = document.querySelector('#genre').value;
+//     const releaseYear = document.querySelector('#releaseYear').value;
+//     const isWatched = document.querySelector('#isWatched').checked;
 
-            <td>${film.title}</td>
-            <td>${film.genre}</td>
-            <td>${film.releaseYear}</td>
-            <td>${film.isWatched ? "Да" : "Нет"}</td>
-        `;
 
-        filmTableBody.appendChild(row);
-    });
-}
+//     const film = {
+//         title,
+//         genre,
+//         releaseYear,
+//         isWatched
+//     }
 
-document.querySelector('#film-form').addEventListener("submit", handleFormSubmit);
+//     console.log(film.title);
 
-renderTable();
+//     addFilmToLocaleStorage(film)
+// }
+
+
+// function addFilmToLocaleStorage(film) {
+//     const films = JSON.parse(localStorage.getItem('films')) || [];
+//     films.push(film)
+//     localStorage.setItem('films', JSON.stringify(films));
+
+//     renderTable();
+// }
+
+// function renderTable() {
+//     const films = JSON.parse(localStorage.getItem('films')) || [];
+//     const filmTableBody = document.querySelector('#film-tbody');
+//     filmTableBody.innerHTML = "";
+
+//     films.forEach((film) => {
+//         const row = document.createElement('tr');
+//         row.innerHTML = `
+
+//             <td>${film.title}</td>
+//             <td>${film.genre}</td>
+//             <td>${film.releaseYear}</td>
+//             <td>${film.isWatched ? "Да" : "Нет"}</td>
+//         `;
+
+//         filmTableBody.appendChild(row);
+//     });
+// }
+
+// document.querySelector('#film-form').addEventListener("submit", handleFormSubmit);
+
+
+// renderTable();
